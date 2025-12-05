8ff13a8b89559c458f2f09311c4405e239a00543
anouar garbaa <g.anouar@yahoo.com>
2025-12-06 00:57:48 +0100

    add createEmptyInvoice Action &  Modal from DaisyUI

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,31 @@
+"use client";
+
 import Wrapper from "./components/Wrapper";
 
 export default function Home() {
   return (
     <Wrapper>
-      <button className="btn btn-accent">test</button>
+      <button
+        className="btn"
+        onClick={() =>
+          (
+            document.getElementById("my_modal_3") as HTMLDialogElement
+          ).showModal()
+        }
+      >
+        open modal
+      </button>
+      <dialog id="my_modal_3" className="modal">
+        <div className="modal-box">
+          <form method="dialog">
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
+          </form>
+          <h3 className="font-bold text-lg">Hello!</h3>
+          <p className="py-4">Press ESC key or click on ✕ button to close</p>
+        </div>
+      </dialog>
     </Wrapper>
   );
 }
